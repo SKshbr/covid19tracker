70a19fb95501a8f0e843bdfda829bd34f33f9c65
SHAIKH SHABBIR MOHAMMED <65480517+SKshbr@users.noreply.github.com>
2020-08-11 08:53:58 +0530

    Updates

---
+++ precache-manifest.4ea08ae05d57cce0ce32ee05dcfc09fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fbf062626736684fc297be99ce35ec2",
+    "url": "/covid19tracker/index.html"
+  },
+  {
+    "revision": "0d5886946c498ff145bc",
+    "url": "/covid19tracker/static/css/main.92fcdaa9.chunk.css"
+  },
+  {
+    "revision": "4bea5f396dae91e507c4",
+    "url": "/covid19tracker/static/js/2.1d0ce562.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19tracker/static/js/2.1d0ce562.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d5886946c498ff145bc",
+    "url": "/covid19tracker/static/js/main.aeea3611.chunk.js"
+  },
+  {
+    "revision": "0a0f313344452cf7e22f",
+    "url": "/covid19tracker/static/js/runtime-main.2f09f3a8.js"
+  }
+]);
